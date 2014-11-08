4cc717aa030b9ed10c7e9599f9f8cc1a6f53a2cd
Frédéric BISSON <zigazou@free.fr>
2014-11-08 10:38:15 +0100

    Corrected MNatural

--- src/Minitel/Type/MNatural.hs
+++ src/Minitel/Type/MNatural.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-|
 Module      : Natural
 Description : Natural numbers for HaMinitel
@@ -9,41 +12,50 @@ Stability   : experimental
 Portability : POSIX
 
 Natural numbers for HaMinitel. The Minitel can only send and receive bytes
-whose values are [0..127]. The MNat data type allows to use such numbers as if
+whose values are [0..127]. The MNatN data type allows to use such numbers as if
 they were standard Int while ensuring that we use only 7 bits.
 -}
 
-module Minitel.Type.MNatural (MNat, mnat, fromMNat) where
+module Minitel.Type.MNatural (MNat, MNatN, mnat, fromMNat) where
 
--- | The MNat type. The constructor is hidden.
-newtype MNat = MakeMNat Int deriving (Real, Eq, Ord, Show)
+import GHC.TypeLits
+import Data.Proxy
 
--- | MNat is a bounded type
-instance Bounded MNat where
-    minBound = MakeMNat 0
-    maxBound = MakeMNat 127
+-- | The MNatN type. The constructor is hidden.
+newtype MNatN (lo :: Nat) (hi :: Nat) = MakeMNat Int
+    deriving (Real, Eq, Ord, Show)
 
--- | Converts an Int into an MNat
-mnat :: Int -> MNat
-mnat x | loLimit <= x && x <= hiLimit = MakeMNat x
-       | otherwise = error "Number out of bounds"
-       where loLimit = fromIntegral (minBound :: MNat)
-             hiLimit = fromIntegral (maxBound :: MNat)
+type MNat = MNatN 0 127
 
--- | Converts an MNat into an Int
-fromMNat :: MNat -> Int
+-- | MNatN is a bounded type
+instance (KnownNat lo, KnownNat hi) => Bounded (MNatN lo hi) where
+    minBound = MakeMNat . fromInteger $ natVal (Proxy :: Proxy lo)
+    maxBound = MakeMNat . fromInteger $ natVal (Proxy :: Proxy hi)
+
+-- | Converts an MNatN into an Int
+fromMNat :: (KnownNat lo, KnownNat hi) => MNatN lo hi -> Int
 fromMNat (MakeMNat i) = i
 
--- | Converts an Int binary function returning Int to a MNat binary function
---   returning an MNat
-mfnat :: (Int -> Int) -> (MNat -> MNat)
+-- | Converts an Int into an MNatN
+mnat :: (KnownNat lo, KnownNat hi) => Int -> MNatN lo hi
+mnat = mnat' minBound maxBound
+    where mnat' :: (KnownNat lo, KnownNat hi) =>
+                   MNatN lo hi -> MNatN lo hi -> Int -> MNatN lo hi
+          mnat' loB hiB x | fromMNat loB <= x && x <= fromMNat hiB = MakeMNat x
+                          | otherwise = error "Number out of bounds"
+
+-- | Converts an Int binary function returning Int to a MNatN binary function
+--   returning an MNatN
+mfnat :: (KnownNat lo, KnownNat hi) =>
+         (Int -> Int) -> (MNatN lo hi -> MNatN lo hi)
 mfnat f = mnat . f . fromMNat
 
-mfnat2 :: (Int -> Int -> Int) -> (MNat -> MNat -> MNat)
+mfnat2 :: (KnownNat lo, KnownNat hi) =>
+          (Int -> Int -> Int) -> (MNatN lo hi -> MNatN lo hi -> MNatN lo hi)
 mfnat2 f x y = mnat $ f (fromMNat x) (fromMNat y)
 
--- | You can do additions, substractions and multiplication with MNat
-instance Num MNat where
+-- | You can do additions, substractions and multiplication with MNatN
+instance (KnownNat lo, KnownNat hi) => Num (MNatN lo hi) where
     fromInteger = mnat . fromIntegral
     (+)         = mfnat2 (+)
     (-)         = mfnat2 (-)
@@ -51,14 +63,13 @@ instance Num MNat where
     abs         = mfnat abs
     signum      = mfnat signum
 
--- | Allows to use toInteger with MNat
-instance Integral MNat where
+-- | Allows to use toInteger with MNatN
+instance (KnownNat lo, KnownNat hi) => Integral (MNatN lo hi) where
     quotRem x y = (fromInteger $ quot x' y', fromInteger $ rem x' y')
                   where (x', y') = (toInteger x, toInteger y)
     toInteger   = toInteger . fromMNat
 
 -- | Allows to generate lists
-instance Enum MNat where
+instance (KnownNat lo, KnownNat hi) => Enum (MNatN lo hi) where
     toEnum      = mnat
     fromEnum    = fromMNat
-
